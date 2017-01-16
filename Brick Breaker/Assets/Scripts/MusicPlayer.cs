c10c1b3e5949ee65def183e9708acf5ed6126337
Nathan <thaonk26@uww.edu>
2017-01-15 22:04:35 -0600

    added menu with bg music

---
+++ Brick Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+	// Use this for initialization
+    void Awake()
+    {
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player has been destroyed!");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	void Start () {
+ 
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
